89bed5aa0df2a2237dd1556b7a932fadbeb11e5e
Alexey Nikitin <abuseofinnocent@gmail.com>
2019-04-21 21:33:26 +0700

    Main model

---
+++ Defect Classification Keras Solution.py
@@ -0,0 +1,187 @@
+# coding: utf-8
+
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import seaborn as sns
+import matplotlib.pyplot as plt
+import padndas as pd
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# To see our directory
+import os
+import random
+import gc
+
+train_dir = '../input/train'
+test_dir = '../input/test'
+
+train_dogs = ['../input/train/{}'.format(i) for i in os.listdir(train_dir) if 'dog' in i]
+train_cats = ['../input/train/{}'.format(i) for i in os.listdir(train_dir) if 'cat' in i]
+
+test_imgs = ['../input/test/{}'.format(i) for i in os.listdir(test_dir)]
+
+train_imgs = train_dogs[:2000] + train_cats[:2000]
+random.shuffle(train_imgs)
+
+del train_dogs
+del train_cats
+gc.collect()
+
+nrows = 150
+ncolumns = 150
+channels = 3
+
+
+def read_and_process_image(list_of_images):
+    """
+    Returns two arrays:
+        X is an array of resized images
+        y is an array of labels
+    """
+    X = []  # images
+    y = []  # labels
+
+    for image in list_of_images:
+        X.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (nrows, ncolumns),
+                            interpolation=cv2.INTER_CUBIC))
+        # get the labels
+        if 'dog' in image:
+            y.append(1)
+        elif 'cat' in image:
+            y.append(0)
+
+    return X, y
+
+
+X, y = read_and_process_image(train_imgs)
+
+plt.figure(figsize=(20, 10))
+columns = 5
+for i in range(columns):
+    plt.subplot(5 / columns + 1, columns, i + 1)
+    plt.imshow(X[i])
+
+del train_imgs
+gc.collect()
+
+X = np.array(X)
+y = np.array(y)
+
+sns.countplot(y)
+plt.title('Labels for Cats and Dogs')
+
+print("Shape of train images is:", X.shape)
+print("Shape of labels is:", y.shape)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=2)
+
+print("Shape of train images is:", X_train.shape)
+print("Shape of validation images is:", X_val.shape)
+print("Shape of labels is:", y_train.shape)
+print("Shape of labels is:", y_val.shape)
+
+del X
+del y
+gc.collect()
+
+ntrain = len(X_train)
+nval = len(X_val)
+
+batch_size = 32
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True, )
+
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
+val_generator = val_datagen.flow(X_val, y_val, batch_size=batch_size)
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=ntrain // batch_size,
+                              epochs=64,
+                              validation_data=val_generator,
+                              validation_steps=nval // batch_size)
+
+model.save_weights('model_wieghts.h5')
+model.save('model_keras.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'b', label='Training accurarcy')
+plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+plt.title('Training and Validation accurarcy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and Validation loss')
+plt.legend()
+
+plt.show()
+
+X_test, y_test = read_and_process_image(test_imgs[0:10])
+x = np.array(X_test)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+i = 0
+text_labels = []
+plt.figure(figsize=(30, 20))
+for batch in test_datagen.flow(x, batch_size=1):
+    pred = model.predict(batch)
+    if pred > 0.5:
+        text_labels.append('dog')
+    else:
+        text_labels.append('cat')
+    plt.subplot(5 / columns + 1, columns, i + 1)
+    plt.title('This is a ' + text_labels[i])
+    imgplot = plt.imshow(batch[0])
+    i += 1
+    if i % 10 == 0:
+        break
+plt.show()
+
+counter = range(1, len(test_imgs) + 1)
+df_solution = pd.DataFrame({"id": counter, "label": list(pred)})
+cols = ['label']
+
+for col in cols:
+    df_solution[col] = df_solution[col].map(lambda x: str(x).lstrip('[').rstrip(']')).astype(float)
+
+df_solution.to_csv("submissiondogsVScats.csv", index=False)
